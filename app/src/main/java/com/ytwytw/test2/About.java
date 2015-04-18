82a18c0af552763dec7e8a05279d46c3b49c222b
Junhua Zhang <zhangjunhuaz@gmail.com>
2015-04-18 18:10:42 -0400

    update 2

--- app/src/main/java/com/ytwytw/test2/About.java
+++ app/src/main/java/com/ytwytw/test2/About.java
@@ -1,9 +1,52 @@
 package com.ytwytw.test2;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by ytwytw on 3/20/15.
  */
 public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        findViewById(R.id.textView2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.linkedin.com/in/ytwytw";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        findViewById(R.id.textView3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://twitter.com/ytwytw";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        findViewById(R.id.textView14).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.ee.ryerson.ca/people/Raahemifar.html";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+    }
+
+
 }
