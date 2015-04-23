e64dbe887cc717d14a5a102e35f9407523389222
ytwytw <zhangjunhuaz@gmail.com>
2015-04-23 01:34:12 -0400

    fix plot graph

--- app/src/main/java/com/ytwytw/test2/Fragment_Plot.java
+++ app/src/main/java/com/ytwytw/test2/Fragment_Plot.java
@@ -11,8 +11,10 @@ import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.XYStepMode;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Fragment_Plot extends Fragment {
 
@@ -28,18 +30,26 @@ public class Fragment_Plot extends Fragment {
         // initialize our XYPlot reference:
         plot = (XYPlot) rootView.findViewById(R.id.mySimpleXYPlot);
 
+
         // Create a couple arrays of y-values to plot:
-        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+        Number[] series1Numbers = {ramgen(), ramgen(), ramgen(), ramgen(), ramgen(), ramgen()};
+        Number[] series2Numbers = {ramgen(), ramgen(), ramgen(), ramgen(), ramgen(), ramgen()};
+        Number[] series3Numbers = {ramgen(), ramgen(), ramgen(), ramgen(), ramgen(), ramgen()};
+        Number[] series4Numbers = {ramgen(), ramgen(), ramgen(), ramgen(), ramgen(), ramgen()};
+        Number[] series5Numbers = {ramgen(), ramgen(), ramgen(), ramgen(), ramgen(), ramgen()};
+//        Number[] series5Numbers = {4, 6, 3, 8, 2, 10};
 
         // Turn the above arrays into XYSeries':
         XYSeries series1 = new SimpleXYSeries(
                 Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
                 SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-                "Series1");                             // Set the display title of the series
+                "CH 1");                             // Set the display title of the series
 
         // same as above
-        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "CH 2");
+        XYSeries series3 = new SimpleXYSeries(Arrays.asList(series3Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "CH 3");
+        XYSeries series4 = new SimpleXYSeries(Arrays.asList(series4Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "CH 4");
+        XYSeries series5 = new SimpleXYSeries(Arrays.asList(series5Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "CH 5");
 
         // Create a formatter to use for drawing a series using LineAndPointRenderer
         // and configure it from xml:
@@ -50,12 +60,30 @@ public class Fragment_Plot extends Fragment {
 
         // same as above:
         LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.CYAN, Color.YELLOW, null, null);
-        // plot.addSeries(series2, series2Format);
+        plot.addSeries(series2, series2Format);
+
+        LineAndPointFormatter series3Format = new LineAndPointFormatter(Color.BLUE, Color.YELLOW, null, null);
+        plot.addSeries(series3, series3Format);
+
+        LineAndPointFormatter series4Format = new LineAndPointFormatter(Color.LTGRAY, Color.YELLOW, null, null);
+        plot.addSeries(series4, series4Format);
+
+        LineAndPointFormatter series5Format = new LineAndPointFormatter(Color.MAGENTA, Color.YELLOW, null, null);
+        plot.addSeries(series5, series5Format);
+
+        plot.setDomainStep(XYStepMode.INCREMENT_BY_VAL,1.0);
+
 
         // reduce the number of range labels
          plot.setTicksPerRangeLabel(3);
          plot.getGraphWidget().setDomainLabelOrientation(-45);
         return rootView;
+    }
+
+    public int ramgen(){
+        Random random = new Random();
+        return (random.nextInt(25)%(25-5+1) + 5);
+
     }
 
 }
